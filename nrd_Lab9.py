fbbeeee7872c87d6e7bf17d21053a8c778f32300
Zachary Lefferts <Lefferts.Zac@gmail.com>
2024-04-11 15:14:36 -0400

    decoder

--- nrd_Lab9.py
+++ nrd_Lab9.py
@@ -5,6 +5,13 @@ def encode(password):
         new_password += str(char)
     return new_password
 
+def decode(password):
+    decoded_password = ""
+    for char in password:
+        char = int(char) - 3
+        decoded_password += str(char)
+    return decoded_password
+
 def main():
     while True:
         print("Menu\n-------------\n1. Encode\n2. Decode\n3. Quit")
@@ -14,6 +21,11 @@ def main():
             password = input("Please enter your password to encode: ")
             new_pass = encode(password)
             print("Your password has been encoded and stored!")
+        if option == 2:
+            decoded_password = decode(new_pass)
+            print(f"The encoded password is {new_pass}, and the original password is {decoded_password}.")
+        if option == 3:
+            break
 
 if __name__ == "__main__":
     main()
